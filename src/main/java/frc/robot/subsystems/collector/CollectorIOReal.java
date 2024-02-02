3ade9e8a396fe9ed56f47d258237e616e6291ddd
BOB Contributer <frc319.git@gmail.com>
2024-02-02 16:11:14 -0500

    Added ShooterIOReal file

---
+++ src/main/java/frc/robot/subsystems/collector/CollectorIOReal.java
@@ -0,0 +1,43 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.collector;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class CollectorIOReal implements CollectorIO {
+  
+  private final CANSparkMax collectorLeftLead = new CANSparkMax(0, MotorType.kBrushless);
+  private final CANSparkMax collectorRightLead = new CANSparkMax(0, MotorType.kBrushless);
+  private final CANSparkMax collectorLeftFollow = new CANSparkMax(0, MotorType.kBrushless);
+  private final CANSparkMax collectorRightFollow = new CANSparkMax(0, MotorType.kBrushless);
+
+
+  public CollectorIOReal() {
+    setCollectorFollow();
+    setCollectorInversions();
+  }
+
+  @Override
+  public void setCollectorPO(double PO) {
+    collectorLeftLead.set(PO);
+    collectorRightLead.set(PO);
+  }
+  
+  public void setCollectorInversions() {
+    collectorLeftLead.setInverted(true);
+    collectorLeftFollow.setInverted(true);
+   
+  }
+
+  public void setCollectorFollow() {
+    collectorLeftFollow.follow(collectorLeftLead);
+    collectorRightFollow.follow(collectorRightLead);
+  }
+}
+
+ 
