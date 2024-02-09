d8e7faf40beee737e4fe778f11462bb6c4353936
BOB Contributer <frc319.git@gmail.com>
2024-02-09 16:11:41 -0500

    Elevator base subsystem with IO's

---
+++ src/main/java/frc/robot/subsystems/elevator/Elevator.java
@@ -0,0 +1,35 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.elevator;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Elevator extends SubsystemBase {
+  private final ElevatorIO io;
+  /** Creates a new Elevator. */
+  public Elevator(ElevatorIO io) {
+    this.io = io;
+
+    switch (Constants.currentMode) {
+      case REAL:
+      case REPLAY:
+      case PROTO:
+      case PROTO2:
+      case SIM:
+      default:
+
+    }
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  public void stop() {
+    io.stop();
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorIOReal.java
@@ -0,0 +1,28 @@
+
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.elevator;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ElevatorIOReal implements ElevatorIO {
+  /** Creates a new ElevatorIOReal. */
+
+  private final CANSparkMax elevatorLead = new CANSparkMax(0, MotorType.kBrushless);
+  private final CANSparkMax elevatorFollow = new CANSparkMax(0, MotorType.kBrushless);
+  public ElevatorIOReal() {}
+
+  @Override
+  public void stop() {}
+
+  /*public void periodic() {
+    // This method will be called once per scheduler run
+  }*/
+
+
+}
