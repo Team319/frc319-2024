03ae9b4741c98156d5c8337776047ba15a1faa6b
BOB Contributer <frc319.git@gmail.com>
2024-02-17 14:03:55 -0500

    populated elevatorioreal subsystem, placed constants in elevatorio subsystem

--- src/main/java/frc/robot/subsystems/elevator/ElevatorIO.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorIO.java
@@ -10,6 +10,22 @@ import org.littletonrobotics.junction.AutoLog;
 /** Add your docs here. */
 public interface ElevatorIO {
 
+    public static final double kPUp = 0.0;  // Power applied to motor
+    public static final double kIUp = 0.0;  // margin of error in motor
+    public static final double kDUp = 0.0;  // Makes the graph line smooth from point A to point B
+    public static final double kFFUp = 0.0; // Feedforward value
+
+    public static final double kPDown = 0.0;  // Power applied to motor
+    public static final double kIDown = 0.0;  // margin of error in motor
+    public static final double kDDown = 0.0;  // Makes the graph line smooth from point A to point B
+    public static final double kFFDown = 0.0; // Feedforward value
+
+    public static final double maxVel = 0.0;
+    public static final double minVel = 0.0;
+    public static final double maxAcc = 0.0;
+    public static final double maxErr = 0.0;
+    public static final int smartMotionSlot = 0;
+
     @AutoLog
     public static class ElevatorIOInputs {}
 
