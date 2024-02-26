0bfc8a4ea5bdbb246ad7715318490c4f000b456e
BOB Contributer <frc319.git@gmail.com>
2024-02-26 12:52:14 -0500

    Fixed merging issues

--- src/main/java/frc/robot/subsystems/elevator/Elevator.java
+++ src/main/java/frc/robot/subsystems/elevator/Elevator.java
@@ -2,16 +2,16 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems.wrist;
+package frc.robot.subsystems.elevator;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.subsystems.shooter.ShooterIO;
 
-public class Wrist extends SubsystemBase {
-  private final WristIO io;
+public class Elevator extends SubsystemBase {
+  private final ElevatorIO io;
   /** Creates a new Wrist. */
-  public Wrist(WristIO io) {
+  public Elevator(ElevatorIO io) {
     this.io = io;
 
     switch (Constants.currentMode) {
